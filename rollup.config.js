422cae9628e50c6bb6ec6bbdb29663de994a0eb6
傅子湘 <fuzx@xmpaoyou.com>
2022-04-19 19:19:11 +0800

    chore: remove ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from "@rollup/plugin-typescript";
+// import typescript from "@rollup/plugin-typescript";
 import pkg from './package.json';
 
 const DIR_DIST = "dist/animate-canvas-lib"
@@ -10,12 +10,12 @@ export default {
 	plugins:[
 		resolve(), // so Rollup can find `ms`
 		commonjs(), // so Rollup can convert `ms` to an ES module
-		typescript(),
+		// typescript(),
 	],
-	// external: ['three'],
-	// globals: {
-		// three: 'THREE',
-	// },
+	external: ['three'],
+	globals: {
+		three: 'THREE',
+	},
 
 	output: [
 		{
@@ -23,14 +23,7 @@ export default {
 			file: DIR_DIST + ".umd.js",
 			format: 'umd',
 			paths: {
-				three: 'https://cdn.jsdelivr.net/npm/three@0.139.2/build/three.min.js',
-			},
-		},
-		{
-			file: DIR_DIST + ".amd.js",
-			format: 'amd',
-			paths: {
-				three: 'https://cdn.jsdelivr.net/npm/three@0.139.2/build/three.min.js',
+				three: 'https://cdn.jsdelivr.net/npm/three@0.86.0/build/three.min.js',
 			},
 		},
 		{ file: DIR_DIST + ".cjs.js", format: 'cjs' },
